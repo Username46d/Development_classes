5e353164fbfd293c941b06764050f844cca0c81f
Username46d <archakovvlad@gmail.com>
2025-10-22 17:38:31 +0300

    Первый коммит

---
+++ Лабораторная1_1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Xml.Linq;
+public class GameEntity
+{
+    public string name;
+    public int health;
+    public bool canTalk;
+    public bool canAttack;
+    public bool canMove;
+    public bool canFly;
+
+    public GameEntity(string name, int health, bool canAttack, bool canTalk, bool canMove, bool canFly)
+    {
+        this.name = name;
+        this.health = health;
+        this.canAttack = canAttack;
+        this.canTalk = canTalk;
+        this.canMove = canMove;
+        this.canFly = canFly;
+    }
+    public void Move()
+    {
+        if (canMove)
+        {
+            Console.WriteLine($"{name} двигается");
+        }
+        else
+        {
+            Console.WriteLine($"{name} не может двигаться");
+        }
+    }
+    public void Attack()
+    {
+        if (canAttack)
+            Console.WriteLine($"{name} атакует");
+        else
+            Console.WriteLine($"{name} не может атаковать");
+    }
+
+    public void Talk()
+    {
+        if (canTalk)
+            Console.WriteLine($"{name} сказал Привет");
+        else
+            Console.WriteLine($"{name} не может говорить");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (health > 0)
+        {
+            health -= damage;
+            Console.WriteLine($"{name} получил {damage} урона. Здоровье: {health}");
+        }
+        else
+        {
+            Console.WriteLine($"{name} уничтожен");
+        }
+    }
+
+    public void Fly()
+    {
+        if (canFly)
+            Console.WriteLine($"{name} летает");
+        else
+            Console.WriteLine($"{name} не может летать");
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"{name} Здоровье: {health}, Умение говорить: {canTalk}, Атака: {canAttack}, Движение: {canMove}, Полет: {canFly}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Задача 1: Монолитный класс ===\n");
+
+        var player = new GameEntity("Player", 100, true, true, true, false);
+        var loader = new GameEntity("Loader", 0, true, false, true, false);
+        var zombie = new GameEntity("Zombie", 50, false, true, true, false);
+
+        Console.WriteLine("Игрок");
+        player.DisplayInfo();
+        player.Talk();
+        player.Move();
+        player.Attack();
+        player.TakeDamage(20);
+
+        Console.WriteLine("\nLoader");
+        loader.DisplayInfo();
+        loader.Talk();
+        loader.Move();
+        loader.Attack();
+
+        Console.WriteLine("\nЗомби");
+        zombie.DisplayInfo();
+        zombie.Talk();
+        zombie.Move();
+        zombie.Attack();
+        zombie.TakeDamage(30);
+
+        Console.WriteLine("\nПриведение");
+        var ghost = new GameEntity("Ghost", 0, canTalk: false, canAttack: true, canMove: false, canFly: true);
+        ghost.DisplayInfo();
+        ghost.Fly();
+        ghost.Attack();
+        ghost.TakeDamage(10);
+    }
+}
---
+++ Лабораторная1_2/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+
+public abstract class GameEntity
+{
+    public abstract void DisplayInfo();
+}
+
+public abstract class Character : GameEntity
+{
+    public int Health;
+    public bool CanMove;
+
+    protected Character(int health, bool canMove)
+    {
+        Health = health;
+        CanMove = canMove;
+    }
+
+    public virtual void Move()
+    {
+        if (CanMove)
+            Console.WriteLine($"{GetType().Name} идет");
+        else
+            Console.WriteLine($"{GetType().Name} не может ходить");
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        if (Health > 0)
+        {
+            Health -= damage;
+            Console.WriteLine($"{GetType().Name} получил {damage} урона. Здоровье: {Health}");
+        }
+    }
+}
+
+public abstract class TalkableCharacter : Character
+{
+    public string Name { get; protected set; }
+    public bool CanTalk { get; protected set; }
+
+    protected TalkableCharacter(string name, int health, bool canMove, bool canTalk)
+        : base(health, canMove)
+    {
+        Name = name;
+        CanTalk = canTalk;
+    }
+
+    public virtual void Talk()
+    {
+        if (CanTalk)
+            Console.WriteLine($"{Name} сказал Привет");
+        else
+            Console.WriteLine($"{GetType().Name} не может говорить");
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Имя: {Name}, Здоровье: {Health}, Умение говорить: {CanTalk}");
+    }
+}
+
+public abstract class AttackingCharacter : Character
+{
+    public bool CanAttack { get; protected set; }
+    public int AttackDamage { get; protected set; }
+
+    protected AttackingCharacter(int health, bool canMove, bool canAttack, int attackDamage = 10)
+        : base(health, canMove)
+    {
+        CanAttack = canAttack;
+        AttackDamage = attackDamage;
+    }
+
+    public virtual void Attack()
+    {
+        if (CanAttack)
+            Console.WriteLine($"{GetType().Name} нанес {AttackDamage} урона!");
+        else
+            Console.WriteLine($"{GetType().Name} не может атаковать");
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Здоровье: {Health}, Атака: {CanAttack}, Урон: {AttackDamage}");
+    }
+}
+
+public class Player : TalkableCharacter
+{
+    public bool CanAttack { get; private set; }
+    public int AttackDamage { get; private set; }
+
+    public Player(string name, int health)
+        : base(name, health, canMove: true, canTalk: true)
+    {
+        CanAttack = true;
+        AttackDamage = 15;
+    }
+
+    public void Attack()
+    {
+        if (CanAttack)
+            Console.WriteLine($"{Name} нанес {AttackDamage} урона!");
+        else
+            Console.WriteLine($"{Name} не может атаковать");
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Игрок: {Name}, Здоровье: {Health}, Атака: {CanAttack}");
+    }
+}
+
+public class Loader : TalkableCharacter
+{
+    public Loader(string name)
+        : base(name, health: 0, canMove: true, canTalk: true)
+    {
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Loader: {Name}, Движение: {CanMove}, Разговорчивость: {CanTalk}");
+    }
+}
+
+public class Zombie : AttackingCharacter
+{
+    public Zombie(int health)
+        : base(health, canMove: true, canAttack: true, attackDamage: 8)
+    {
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"ЗомбиЗдоровье: {Health}, Атака: {CanAttack}");
+    }
+}
+
+public class Box : GameEntity
+{
+    public int Health { get; private set; }
+
+    public Box(int health)
+    {
+        Health = health;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Console.WriteLine($"Коробка получила {damage} урона. Здоровье: {Health}");
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Коробка Здоровье: {Health}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var player = new Player("John", 100);
+        var loader = new Loader("Bob");
+        var zombie = new Zombie(50);
+        var box = new Box(20);
+
+        Console.WriteLine("Player");
+        player.DisplayInfo();
+        player.Move();
+        player.Talk();
+        player.Attack();
+
+        Console.WriteLine("\nLoader");
+        loader.DisplayInfo();
+        loader.Move();
+        loader.Talk();
+
+        Console.WriteLine("\nZombie");
+        zombie.DisplayInfo();
+        zombie.Move();
+        zombie.Attack();
+
+        Console.WriteLine("\nBox");
+        box.DisplayInfo();
+        box.TakeDamage(5);
+
+    }
+}
